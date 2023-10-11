182d3b1346af55a35650d0f3cd88d9a2c890df7c
mylyyyyy <93071012+mylyyyyy@users.noreply.github.com>
2023-10-11 22:32:30 +0700

    Add files via upload
    praktikum ui

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic/widget/textwidget.dart';
+import 'package:flutter_basic/layout/alignment.dart';
+import 'package:flutter_basic/rowandcolumn/rowwidget.dart';
+import 'package:flutter_basic/stack/stack.dart';
+import 'package:flutter_basic/list/listview.dart';
+import 'package:flutter_basic/grid/gridview.dart';
+import 'package:flutter_basic/tugas/tugas.dart';
+import 'package:flutter_basic/layout/child.dart';
+import 'package:flutter_basic/layout/color.dart';
+import 'package:flutter_basic/layout/heightandwidth.dart';
+
+void main() {
+  runApp(const Praktikum());
+}
